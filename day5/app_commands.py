4945b8603b045526759a7722efece15a3af6e676
A9312 <akshitgupta8368@gmail.com>
2026-05-25 07:51:03 +0530

    third Commit

---
+++ day5/app_commands.py
@@ -0,0 +1,12 @@
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+print(driver.title)
+
+print(driver.current_url)
+
+print(driver.page_source)
