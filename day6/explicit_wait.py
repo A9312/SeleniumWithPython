36297025f9efb969a89d262b844f9ee4a57127ca
A9312 <akshitgupta8368@gmail.com>
2026-05-25 23:40:23 +0530

    eighth Commit

---
+++ day6/explicit_wait.py
@@ -0,0 +1,29 @@
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+# mywait = WebDriverWait(driver,10)
+
+mywait = WebDriverWait(driver, 10,ignored_exceptions=[NoSuchElementException,Exception])
+
+driver.get("https://www.google.com/")
+
+driver.maximize_window()
+
+# time.sleep(2)
+
+search_box=driver.find_element(By.NAME,"q")
+
+search_box.send_keys("Selenium")
+
+search_box.submit()
+
+search_link = mywait.until(EC.presence_of_element_located((By.XPATH,"//h3[text()='Selenium']")))
+
+search_link.click()
---
+++ day6/implicit_wait.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.implicitly_wait(10)
+
+driver.get("https://www.google.com/")
+
+driver.maximize_window()
+
+# time.sleep(2)
+
+search_box=driver.find_element(By.NAME,"q")
+
+search_box.send_keys("Selenium")
+
+search_box.submit()
+
+# time.sleep(2)
+
+driver.find_element(By.XPATH,"//h3[text()='Selenium']").click()
+
+# time.sleep(2)
